5b058c2c87ee6ad4e2855a5b7a0c4fa678cacd6c
Ranjitha1304 <ranjitha13cs@gmail.com>
2025-09-14 23:03:33 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
+
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL|| 'http://127.0.0.1:8000/api/';
+
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000/api"  // Django backend URL
+  baseURL: BASE_URL
 });
